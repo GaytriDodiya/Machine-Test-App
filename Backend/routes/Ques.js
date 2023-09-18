915e634e079a09bf4fc3e66594a5a55d22749df6
karansharma11 <133631589+karansharma11@users.noreply.github.com>
2023-09-18 16:47:03 +0530

    Add 'candidate-test-app' to the repository

--- Backend/routes/Ques.js
+++ Backend/routes/Ques.js
@@ -25,4 +25,31 @@ router.get('/show-questions', async (req, res) => {
   }
 });
 
+router.post('/result', async (req, res) => {
+  const userResponse = req.body.response;
+  const Noofques = userResponse.length;
+  let rightans = 0;
+  let wrongans = 0;
+
+  // Use Promise.all to wait for all asynchronous operations to complete
+  await Promise.all(
+    userResponse.map(async (item) => {
+      try {
+        const ques = await Ques.findById(item._id);
+        if (ques.correctAnswer == item.correctAnswer) {
+          rightans++;
+        } else {
+          wrongans++;
+        }
+      } catch (error) {
+        console.error('Error fetching question:', error);
+        wrongans++; // Treat errors as wrong answers
+      }
+    })
+  );
+
+  // Send the results as an object
+  res.status(200).json({ rightans, wrongans, Noofques });
+});
+
 module.exports = router;
