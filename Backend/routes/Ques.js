714935aed09671e37a16fd6d7fd9d59965ae3e06
karansharma11 <133631589+karansharma11@users.noreply.github.com>
2023-09-18 14:54:38 +0530

    Question api

--- Backend/routes/Ques.js
+++ Backend/routes/Ques.js
@@ -1,8 +1,28 @@
 const router = require('express').Router();
 const Ques = require('../models/Ques');
 
-router.get('/karan', (req, res) => {
-  res.send('karan login');
+router.post('/upload-question', async (req, res) => {
+  try {
+    const newQues = new Ques({
+      language: req.body.language,
+      question: req.body.question,
+      options: req.body.options,
+      correctAnswer: req.body.correctAnswer,
+    });
+    await newQues.save();
+    res.status(200).json(newQues);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/show-questions', async (req, res) => {
+  try {
+    const Questions = await Ques.find();
+    res.send(Questions);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
