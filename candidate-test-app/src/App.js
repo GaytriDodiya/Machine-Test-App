8e24a9622901d4e5f67bd4406f9e22e5ad9e621d
Gaytri Dodiya <dodiyadsvv@gmail.com>
2023-09-20 10:54:31 +0530

    protect all pages from unauthorized user

--- candidate-test-app/src/App.js
+++ candidate-test-app/src/App.js
@@ -6,10 +6,11 @@ import AdminDashboardScreen from './Screens/AdminDashboardScreen';
 import AdminViewUsersScreen from './Screens/AdminViewUsersScreen';
 import AdminGenerateLoginCodeScrren from './Screens/AdminGenerateLoginCodeScrren';
 import AdminCreateUserScrren from './Screens/AdminCreateUserScrren';
-import { Button, Col, Image, Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Sidebar from './Components/sidebar';
 import { useContext } from 'react';
 import { Store } from './Store';
+import ProtectedRoutes from './Components/protectedRoutes';
 function App() {
   const { state } = useContext(Store);
   const { userInfo } = state;
@@ -26,19 +27,17 @@ function App() {
             <Col md={10} >
               <Routes>
                 <Route path='/' element={<LoginScreen />} />
-                <Route path='/homeScreen' element={<HomeScreen />} />
-                <Route path='/quiz' element={<QuizScreen />} />
-                <Route path="/adminDashbord" element={<AdminDashboardScreen />} />
-                <Route path="/adminViewUsers" element={<AdminViewUsersScreen />} />
-                <Route path="/adminGenerateLoginCode" element={<AdminGenerateLoginCodeScrren />} />
-                <Route path="/adminCreateUser" element={<AdminCreateUserScrren />} />
+                <Route path='/homeScreen' element={<ProtectedRoutes><HomeScreen /></ProtectedRoutes>} />
+                <Route path='/quiz' element={<ProtectedRoutes><QuizScreen /></ProtectedRoutes>} />
+                <Route path="/adminDashbord" element={<ProtectedRoutes><AdminDashboardScreen /></ProtectedRoutes>} />
+                <Route path="/adminViewUsers" element={<ProtectedRoutes><AdminViewUsersScreen /></ProtectedRoutes>} />
+                <Route path="/adminGenerateLoginCode" element={<ProtectedRoutes><AdminGenerateLoginCodeScrren /></ProtectedRoutes>} />
+                <Route path="/adminCreateUser" element={<ProtectedRoutes><AdminCreateUserScrren /></ProtectedRoutes>} />
               </Routes>
             </Col>
           </Row>
         </div>
       </BrowserRouter>
-
-
     </div>
   );
 }
